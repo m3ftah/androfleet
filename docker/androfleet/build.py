5f3c7435ab2bd3195037669d60da961627a927c4
Romain SOMMERARD <rsommerard@gmail.com>
2016-08-02 16:51:49 +0200

    Add androfleet builder

--- docker/androfleet/build.py
+++ docker/androfleet/build.py
@@ -4,11 +4,58 @@ import os
 import subprocess
 import argparse
 import json
+import shutil
+
+APP = 'GPSLocation'
+
+THIS = "/".join(os.path.realpath(__file__).split('/')[:-1])
+CORE = THIS + '/../../core'
+MASTER = CORE + '/master'
+NODE = CORE + '/node'
+SERVDISC = CORE + '/servicediscovery'
+UI = CORE + '/ui'
+ANDROID = THIS + '/../../android/' + APP
 
 parser = argparse.ArgumentParser(prog='build.py', description='Docker androfleet container builder')
 parser.add_argument('-p', '--push', action='store_true')
 args = parser.parse_args()
 
+# Android
+os.chdir(ANDROID)
+subprocess.call(['./gradlew', 'clean', 'assembleDebug'])
+if os.path.exists(THIS + '/app-debug.apk'):
+    os.remove(THIS + '/app-debug.apk')
+shutil.copy(ANDROID + '/app/build/outputs/apk/app-debug.apk', THIS)
+
+# Master
+os.chdir(MASTER)
+subprocess.call(['sbt', 'clean', 'universal:packageBin'])
+if os.path.exists(THIS + '/androfleet-master-1.0.zip'):
+    os.remove(THIS + '/androfleet-master-1.0.zip')
+shutil.copy(MASTER + '/target/universal/androfleet-master-1.0.zip', THIS)
+
+# Node
+os.chdir(NODE)
+subprocess.call(['sbt', 'clean', 'universal:packageBin'])
+if os.path.exists(THIS + '/androfleet-node-1.0.zip'):
+    os.remove(THIS + '/androfleet-node-1.0.zip')
+shutil.copy(NODE + '/target/universal/androfleet-node-1.0.zip', THIS)
+
+# ServiceDiscovery
+os.chdir(SERVDISC)
+subprocess.call(['sbt', 'clean', 'universal:packageBin'])
+if os.path.exists(THIS + '/androfleet-servicediscovery-1.0.zip'):
+    os.remove(THIS + '/androfleet-servicediscovery-1.0.zip')
+shutil.copy(SERVDISC + '/target/universal/androfleet-servicediscovery-1.0.zip', THIS)
+
+# UI
+os.chdir(UI)
+subprocess.call(['sbt', 'clean', 'universal:packageBin'])
+if os.path.exists(THIS + '/androfleet-ui-1.0.zip'):
+    os.remove(THIS + '/androfleet-ui-1.0.zip')
+shutil.copy(UI + '/target/universal/androfleet-ui-1.0.zip', THIS)
+
+os.chdir(THIS)
 subprocess.call(['docker', 'build', '-t', 'rsommerard/androfleet', '.'])
 
 if args.push:
@@ -21,3 +68,15 @@ if args.push:
                 subprocess.call(['docker', 'login'])
 
     subprocess.call(['docker', 'push', 'rsommerard/androfleet'])
+
+# Clean
+if os.path.exists(THIS + '/app-debug.apk'):
+    os.remove(THIS + '/app-debug.apk')
+if os.path.exists(THIS + '/androfleet-master-1.0.zip'):
+    os.remove(THIS + '/androfleet-master-1.0.zip')
+if os.path.exists(THIS + '/androfleet-node-1.0.zip'):
+    os.remove(THIS + '/androfleet-node-1.0.zip')
+if os.path.exists(THIS + '/androfleet-servicediscovery-1.0.zip'):
+    os.remove(THIS + '/androfleet-servicediscovery-1.0.zip')
+if os.path.exists(THIS + '/androfleet-ui-1.0.zip'):
+    os.remove(THIS + '/androfleet-ui-1.0.zip')
---
+++ docker/androfleet/entrypoint.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+  echo "Usage : $0 ContainerType"
+  exit
+fi
+
+MODE=$1
+IP=$(ip addr list eth0 | grep 'inet ' | cut -d ' ' -f6 | cut -d/ -f1)
+
+function node {
+  if [ $# -lt 2 ]
+  then
+    echo "Usage : $0 node Package/Activity"
+    exit
+  fi
+
+  ME=$(ip route | grep 'src 10.' | awk '{print $NF;exit}')
+
+  echo "WEAVE_IP: $ME"
+
+  echo "Configuring redir for $ME..."
+  redir --laddr=$ME --lport=11131 --caddr=127.0.0.1 --cport=11131 &
+
+  ./androfleet-node-1.0/bin/androfleet-node $2 $ME &
+
+  echo 'Starting emulator[5554]...'
+  emulator64-x86 -avd Androidx86 -no-skin -no-audio -no-window -no-boot-anim -noskin -gpu off -port 5554 &
+
+  echo 'Waiting for emulator to start...'
+  BOOT_COMPLETED=''
+  FAIL_COUNTER=0
+  until [[ "$BOOT_COMPLETED" =~ '1' ]]; do
+     BOOT_COMPLETED=`adb -s emulator-5554 shell getprop sys.boot_completed 2>&1`
+     if [[ "$BOOT_COMPLETED" =~ 'not found' ]]; then
+        let 'FAIL_COUNTER += 1'
+        if [[ $FAIL_COUNTER -gt 120 ]]; then
+          echo '  Failed to start emulator'
+          exit 1
+        fi
+     fi
+     sleep 1
+  done
+
+  echo 'Emulator started'
+
+  echo 'Adding emulator redirections...'
+  echo "" > ~/.emulator_console_auth_token
+  (echo 'auth ""'; sleep 1; echo "redir add tcp:11131:11131"; sleep 1; echo 'exit') | telnet localhost 5554
+
+  echo 'Installing the apk...'
+  adb -e install -r /app-debug.apk
+  adb -e logcat -c
+
+  echo 'Launching application...'
+  adb -e shell am start -n $1
+
+  echo 'Running...'
+  adb -e logcat | grep WiDi
+}
+
+case $MODE in
+  'master' )
+    if [ $# -lt 2 ]
+    then
+      echo "Usage : $0 master NbNodes"
+      exit
+    fi
+
+    ./androfleet-master-1.0/bin/androfleet-master $2 ;;
+  'node' )
+    if [ $# -lt 2 ]
+    then
+      echo "Usage : $0 node Package/Activity"
+      exit
+    fi
+
+    ME=$(ip route | grep 'src 10.' | awk '{print $NF;exit}')
+
+    echo "WEAVE_IP: $ME"
+
+    echo "Configuring redir for $ME..."
+    redir --laddr=$ME --lport=11131 --caddr=127.0.0.1 --cport=11131 &
+
+    ./androfleet-node-1.0/bin/androfleet-node $2 $ME &
+
+    echo 'Starting emulator[5554]...'
+    emulator64-x86 -avd Androidx86 -no-skin -no-audio -no-window -no-boot-anim -noskin -gpu off -port 5554 &
+
+    echo 'Waiting for emulator to start...'
+    BOOT_COMPLETED=''
+    FAIL_COUNTER=0
+    until [[ "$BOOT_COMPLETED" =~ '1' ]]; do
+      BOOT_COMPLETED=`adb -s emulator-5554 shell getprop sys.boot_completed 2>&1`
+      if [[ "$BOOT_COMPLETED" =~ 'not found' ]]; then
+        let 'FAIL_COUNTER += 1'
+        if [[ $FAIL_COUNTER -gt 120 ]]; then
+          echo '  Failed to start emulator'
+          exit 1
+        fi
+      fi
+      sleep 1
+    done
+
+    echo 'Emulator started'
+
+    echo 'Adding emulator redirections...'
+    echo "" > ~/.emulator_console_auth_token
+    (echo 'auth ""'; sleep 1; echo "redir add tcp:11131:11131"; sleep 1; echo 'exit') | telnet localhost 5554
+
+    echo 'Installing the apk...'
+    adb -e install -r /app-debug.apk
+    adb -e logcat -c
+
+    echo 'Launching application...'
+    adb -e shell am start -n $1
+
+    echo 'Running...'
+    adb -e logcat | grep WiDi ;;
+  'servicediscovery' )
+    ./androfleet-servicediscovery-1.0/bin/androfleet-servicediscovery ;;
+  'ui' )
+    echo "Configuring redir for $IP..."
+    redir --laddr=$IP --lport=8080 --caddr=127.0.0.1 --cport=8080 &
+
+    ./androfleet-ui-1.0/bin/androfleet-ui ;;
+  * )
+    echo "$MODE is not recognized as ContainerType"
+    exit ;;
+esac
