b8608acac29fbd46ddc1649a02b9a72e7358fe1e
Lakhdar Meftah - Vortex <guemar93@gmail.com>
2017-03-01 11:31:17 +0000

    change dependecy on path

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -4,6 +4,8 @@ import subprocess
 import os
 import sys
 import time
+from os.path import expanduser
+home = expanduser("~")
 
 print('Launching experiment')
 
@@ -11,21 +13,21 @@ NB_NODES = sys.argv[1]
 
 subprocess.call(['pwd'])
 print("Cleaning...")
-subprocess.call(['/home/lakhdar/ilab/androfleet/experiment/clean.py'])
+subprocess.call([home + '/ilab/androfleet/experiment/clean.py'])
 
 print("Launching database")
-subprocess.call(['/home/lakhdar/ilab/androfleet/experiment/db.py'])
+subprocess.call([home + '/ilab/androfleet/experiment/db.py'])
 
 print("Launching Master")
-subprocess.call(['/home/lakhdar/ilab/androfleet/experiment/master.py', NB_NODES])
+subprocess.call([home + '/ilab/androfleet/experiment/master.py', NB_NODES])
 
 time.sleep(5)
 
 print("Launching Service Discovery")
-subprocess.call(['/home/lakhdar/ilab/androfleet/experiment/servicediscovery.py'])
+subprocess.call([home + '/ilab/androfleet/experiment/servicediscovery.py'])
 
 print("Launching Service Discovery")
-subprocess.call(['/home/lakhdar/ilab/androfleet/experiment/node.py',NB_NODES])
+subprocess.call([home + '/ilab/androfleet/experiment/node.py',NB_NODES])
 
 print("androfleet-master log :")
 subprocess.call(['docker','logs','-f','androfleet-master'])
